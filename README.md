e4edec23b85f70d28a277d311a789bf46c5e7cc0
Aditya Panda <adipanda@Adityas-MacBook-Pro.local>
2023-02-25 16:37:21 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kuebiko --> AI Virtual Streamer
+
+A Twitch Chat Bot that reads twitch chat and creates a text to speech response using google could api and openai's GPT-3 text completion model.
+
+### Instructions
+
+Replace API Keys in code and add google cloud json file and program should work!
---
+++ chat.py
@@ -0,0 +1,29 @@
+import openai
+
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return infile.read()
+
+
+openai.api_key = ""
+
+
+def gpt3_completion(prompt, engine='text-davinci-003', temp=0.9, tokens=400, freq_pen=2.0, pres_pen=2.0, stop=['DOGGIEBRO:', 'CHATTER:']):
+    prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
+    response = openai.Completion.create(
+        engine=engine,
+        prompt=prompt,
+        temperature=temp,
+        max_tokens=tokens,
+        frequency_penalty=freq_pen,
+        presence_penalty=pres_pen,
+        stop=stop)
+    text = response['choices'][0]['text'].strip()
+    return text
+
+
+
+
+
+
