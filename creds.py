85472384b0d01b702ee1ae7a6edde42e543e4381
Aditya Panda <adipanda@wireless-10-155-34-148.public.utexas.edu>
2023-04-03 15:49:09 -0500

    fixed bug

--- creds.py
+++
@@ -1,6 +0,0 @@
-# You're Twitch Token 
-TWITCH_TOKEN = "cti77y4533xwoo1yl0uo8zgyujdum2"
-# Your OpenAI API Key
-OPENAI_API_KEY = "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"
-# Your Google Cloud JSON Path
-GOOGLE_JSON_PATH = "wired-strategy-379023-3cb007372512.json"
