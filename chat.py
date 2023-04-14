0d5fe4f57d9fbb81d0e6efb46af9eb2f1fddd7b8
Edgar Valdez <edgarvaldez99@gmail.com>
2023-04-13 23:09:44 -0400

    refactor: config .env file

--- chat.py
+++ chat.py
@@ -3,15 +3,23 @@ import creds
 
 
 def open_file(filepath):
-    with open(filepath, 'r', encoding='utf-8') as infile:
+    with open(filepath, "r", encoding="utf-8") as infile:
         return infile.read()
 
 
 openai.api_key = creds.OPENAI_API_KEY
-openai.api_base = 'https://api.openai.com/v1/chat'
+openai.api_base = "https://api.openai.com/v1/chat"
 
 
-def gpt3_completion(messages, engine='gpt-3.5-turbo', temp=0.9, tokens=400, freq_pen=2.0, pres_pen=2.0, stop=['DOGGIEBRO:', 'CHATTER:']):
+def gpt3_completion(
+    messages,
+    engine="gpt-3.5-turbo",
+    temp=0.9,
+    tokens=400,
+    freq_pen=2.0,
+    pres_pen=2.0,
+    stop=["DOGGIEBRO:", "CHATTER:"],
+):
     response = openai.Completion.create(
         model=engine,
         messages=messages,
@@ -19,6 +27,7 @@ def gpt3_completion(messages, engine='gpt-3.5-turbo', temp=0.9, tokens=400, freq
         max_tokens=tokens,
         frequency_penalty=freq_pen,
         presence_penalty=pres_pen,
-        stop=stop)
-    text = response['choices'][0]['message']['content'].strip()
+        stop=stop,
+    )
+    text = response["choices"][0]["message"]["content"].strip()
     return text
--- creds.py
+++ creds.py
@@ -1,8 +1,10 @@
-# You're Twitch Token 
-TWITCH_TOKEN = ""
+from decouple import config as environ  # type: ignore
+
+# You're Twitch Token
+TWITCH_TOKEN = str(environ("TWITCH_TOKEN", ""))
 # Your TWITCH Channel Name
-TWITCH_CHANNEL = ""
+TWITCH_CHANNEL = str(environ("TWITCH_CHANNEL", ""))
 # Your OpenAI API Key
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = str(environ("OPENAI_API_KEY", ""))
 # Your Google Cloud JSON Path
-GOOGLE_JSON_PATH = ""
+GOOGLE_JSON_PATH = str(environ("GOOGLE_JSON_PATH", ""))
