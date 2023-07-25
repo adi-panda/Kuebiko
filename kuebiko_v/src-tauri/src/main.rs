c6cb9f64ffbf7dad8a249ea1ee1f6a72ae498dbd
Aditya Panda <pandaaditya48@gmail.com>
2023-07-25 17:50:25 -0500

    Global State Build

--- kuebiko_v/src-tauri/src/main.rs
+++ kuebiko_v/src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+  fix_path_env::fix();
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
