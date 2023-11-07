e2132ed94f3adec6f8bb95b22a331a05ad3dd02f
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2023-11-07 02:41:57 -0800

    Moved some config options over here
    Easier to modify!

--- creds.py
+++ creds.py
@@ -6,3 +6,9 @@ TWITCH_CHANNEL = ""
 OPENAI_API_KEY = ""
 # Your Google Cloud JSON Path
 GOOGLE_JSON_PATH = ""
+# Your TWITCH BOT CHANNEL Token (Copy TWITCH_TOKEN if same channel as TWITCH_CHANNEL)
+BOT_ACCOUNT_TWITCH_OAUTH = ""
+# Your TWITCH BOT CHANNEL Name - This is the channel that will post messages to chat
+BOT_ACCOUNT_TWITCH_CHANNEL = ""
+# The channel to send the bot's messages to this channel
+SENDMESSAGE_TO_THIS_CHANNEL = ""
