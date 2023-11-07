eb5732df942be867f05122be7f22c50f8f2c4ac1
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2023-11-07 05:11:36 -0800

    Update blocklist.py
    More entries about what to write here and how

--- blocklist.py
+++ blocklist.py
@@ -1,3 +1,4 @@
-# Enter blocked names as such: blocked_names = ["User1", "User2", ...]
+# Enter blocked names as such: blocked_names = ["user1", "user2", ...]
 # Any usernames entered here will be prevented from interacting with the bot
+# All names must be lower-case, regardless of how they display on Twitch
 blocked_names = []
