3779485abea74caec66fdfd09429ef19010e8ece
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2023-11-12 00:42:06 -0800

    Use 3.5-turbo-1106 instead

--- chat.py
+++ chat.py
@@ -10,7 +10,6 @@ def open_file(filepath):
 openai.api_key = creds.OPENAI_API_KEY
 openai.api_base = 'https://api.openai.com/v1/chat'
 
-
 def gpt3_completion(messages, engine='gpt-3.5-turbo-1106', temp=1.2, tokens=210, freq_pen=0.0, pres_pen=0.0, stop=['AINAME:', 'CHATTER:']):
     response = openai.Completion.create(
         model=engine,
