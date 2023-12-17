acf96837c978bde7a73297eeac8d64d3afa4993e
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2023-12-17 04:20:08 -0800

    Moved over chat.py settings to settings.py

--- chat.py
+++ chat.py
@@ -1,6 +1,6 @@
 import openai
 import creds
-
+import settings
 
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
@@ -10,12 +10,13 @@ def open_file(filepath):
 openai.api_key = creds.OPENAI_API_KEY
 openai.api_base = 'https://api.openai.com/v1/chat'
 
-def gpt3_completion(messages, engine='gpt-3.5-turbo-1106', temp=1.2, tokens=210, freq_pen=0.0, pres_pen=0.0, stop=['AINAME:', 'CHATTER:']):
+def gpt3_completion(messages, engine=settings.chatEngine, topp=max(min(float(settings.chatTopp), 1), 0) ,temp=max(min(float(settings.chatTemperature), 2), 1), tokens=abs(int(settings.chatTokenCount)), freq_pen=max(min(float(settings.chatFreqPenalty), 2), 1), pres_pen=max(min(float(settings.chatPresPenalty), 2), 1), stop=settings.chatStop):
     response = openai.Completion.create(
         model=engine,
         messages=messages,
         temperature=temp,
         max_tokens=tokens,
+        top_p=topp,
         frequency_penalty=freq_pen,
         presence_penalty=pres_pen,
         stop=stop)
